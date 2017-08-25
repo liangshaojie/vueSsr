fc7428405dc7f3d9030b6334153ddecab66392bd
877942720@qq.com <lsj890305>
2017-08-25 23:26:53 +0800

    6666

--- layouts/error.vue
+++ layouts/error.vue
@@ -8,7 +8,7 @@
       {{ error.message }}
     </h2>
     <nuxt-link class="button" to="/" v-if="error.statusCode === 404">
-      Homepage
+      Homepage555555
     </nuxt-link>
   </section>
 </template>
