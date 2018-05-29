e9ada8539f8693494c2f3d73690c59b71f61a784
liangshaojie <877942720@qq.com>
2018-05-29 22:06:33 +0800

    公众号介入成功了

--- server/middlewares/router.js
+++ server/middlewares/router.js
@@ -3,21 +3,21 @@ import config from '../config'
 import sha1 from 'sha1'
 
 export const router = app => {
-  const router = new Router()
-  router.get('/wechat-head', (ctx,next) => {
-    const token = config.wechat.token
-    let {signature,nonce,timestamp, echostr} = ctx.query
-    const str = [token,timestamp,nonce].sort().join('')
-    const sha = sha1(str)
-    if(sha === signature){
-      ctx.body = echostr
-    }else{
-      ctx.body = 'fail'
-    }
-  })
-  // router.post('/wechat-head', (ctx,next) => {
-  // 
-  // })
-  app.use(router.routes())
-  app.use(router.allowedMethods())
+    const router = new Router()
+    router.get('/wechat-head', (ctx, next) => {
+        const token = config.wechat.token
+        let {signature, nonce, timestamp, echostr} = ctx.query
+        const str = [token, timestamp, nonce].sort().join('')
+        const sha = sha1(str)
+        if (sha === signature) {
+            ctx.body = echostr
+        } else {
+            ctx.body = 'fail'
+        }
+    })
+    // router.post('/wechat-head', (ctx,next) => {
+    //
+    // })
+    app.use(router.routes())
+    app.use(router.allowedMethods())
 }
